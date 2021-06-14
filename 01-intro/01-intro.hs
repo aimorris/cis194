8830a7d69e0c5f29b6e1dcd4f42f5416d4f1d62f
Adam Morris <aimorris@aimorris.com>
2021-06-15 09:57:26 +1000

    01-intro credit card complete

--- 01-intro/01-intro.hs
+++ 01-intro/01-intro.hs
@@ -2,7 +2,23 @@
 
 toDigits :: Integer -> [Integer]
 toDigits 0 = []
-toDigits n = toDigits (div n 10) ++ [mod n 10]
+toDigits n 
+  | n < 0 = []
+  | otherwise = toDigits (div n 10) ++ [mod n 10]
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev n = reverse (toDigits n)
+toDigitsRev = reverse . toDigits
+
+doubleEveryOtherRev :: [Integer] -> [Integer]
+doubleEveryOtherRev [] = []
+doubleEveryOtherRev [x] = [x]
+doubleEveryOtherRev (x:y:z) = x : y*2 : doubleEveryOtherRev z
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther = reverse . doubleEveryOtherRev . reverse
+
+sumDigits :: [Integer] -> Integer
+sumDigits = sum . concatMap toDigits
+
+validate :: Integer -> Bool
+validate x = mod (sumDigits (doubleEveryOther (toDigits x))) 10 == 0
