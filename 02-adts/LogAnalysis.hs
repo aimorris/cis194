9442ef362926fcbdd07cdaffe34509a682530146
Adam Morris <aimorris@aimorris.com>
2021-06-16 12:55:43 +1000

    02-adts

--- 02-adts/LogAnalysis.hs
+++ 02-adts/LogAnalysis.hs
@@ -2,6 +2,9 @@
 module LogAnalysis where
 import Log
 
+test :: [LogMessage]
+test = [LogMessage Info 6 "Completed armadillo processing",LogMessage Info 1 "Nothing to report",LogMessage Info 4 "Everything normal",LogMessage Info 11 "Initiating self-destruct sequence",LogMessage (Error 70) 3 "Way too many pickles",LogMessage (Error 65) 8 "Bad pickle-flange interaction detected",LogMessage Warning 5 "Flange is due for a check-up",LogMessage Info 7 "Out for lunch, back in two time steps",LogMessage (Error 20) 2 "Too many pickles",LogMessage Info 9 "Back from lunch",LogMessage (Error 99) 10 "Flange failed!"]
+
 parseMessage :: String -> LogMessage
 parseMessage string =
   case words string of
@@ -29,3 +32,16 @@ build = foldr insert Leaf
 inOrder :: MessageTree -> [LogMessage]
 inOrder Leaf = []
 inOrder (Node left middle right) = inOrder left ++ [middle] ++ inOrder right
+
+filterError :: LogMessage -> Bool
+filterError (LogMessage (Error sev) _ _)
+  | sev > 50 = True
+  | otherwise = False
+filterError _ = False
+
+extractString :: LogMessage -> String
+extractString (LogMessage _ _ msg) = msg
+extractString (Unknown msg) = msg
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong x = map extractString $ filter filterError $ inOrder $ build x
