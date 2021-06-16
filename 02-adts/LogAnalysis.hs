b5bf2c1789bbb5d0c77a2c2521cc22f68c353c33
Adam Morris <aimorris@aimorris.com>
2021-06-16 12:09:00 +1000

    inOrder

--- 02-adts/LogAnalysis.hs
+++ 02-adts/LogAnalysis.hs
@@ -9,3 +9,23 @@ parseMessage string =
     ("W":y:ys) -> LogMessage Warning (read y) (unwords ys)
     ("E":y:z:zs) -> LogMessage (Error (read y)) (read z) (unwords zs)
     x -> Unknown (unwords x)
+
+parse :: String -> [LogMessage]
+parse = map parseMessage . lines
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert
+  x@(LogMessage _ insertTime _)
+  (Node leftTree middle@(LogMessage _ middleTime _) rightTree) =
+    if insertTime > middleTime
+    then Node leftTree middle (insert x rightTree)
+    else Node (insert x leftTree) middle rightTree
+insert x Leaf = Node Leaf x Leaf
+insert _ y = y
+
+build :: [LogMessage] -> MessageTree
+build = foldr insert Leaf
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node left middle right) = inOrder left ++ [middle] ++ inOrder right
