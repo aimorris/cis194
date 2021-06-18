64403d329bbf2689770dbeea2b0ab3950f4f8aa3
Adam Morris <aimorris@aimorris.com>
2021-06-18 18:05:18 +1000

    histogram

--- 03-rec-poly/Golf.hs
+++ 03-rec-poly/Golf.hs
@@ -7,7 +7,21 @@ import Data.List.Split
 skips :: [a] -> [[a]]
 skips a = map (everyNth a) [1..length a]
 
+localMaxima :: [Integer] -> [Integer]
+localMaxima (x:y:z:zs) =
+  if x < y && y > z then y : localMaxima (y:z:zs) else localMaxima (y:z:zs)
+localMaxima _ = []
+
 -- Helper functions
 
 everyNth :: [a] -> Int -> [a]
 everyNth a n = concatMap (drop (n-1)) (chunksOf n a)
+
+histogram :: [Integer] -> String
+histogram x = unlines (map (oneLine (count x)) [9,8..1]) ++ "==========\n0123456789\n"
+
+oneLine :: [Int] -> Int -> String
+oneLine x y = map (\n -> if n >= y then '*' else ' ') x
+
+count :: [Integer] -> [Int]
+count x = map (\n -> length (filter (== n) x)) [0..9]
