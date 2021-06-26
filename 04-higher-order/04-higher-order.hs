5dd115be849c08e6906a4240442a642298c86cdc
Adam Morris <aimorris@aimorris.com>
2021-06-26 13:06:35 +1000

    fun2

--- 04-higher-order/04-higher-order.hs
+++ 04-higher-order/04-higher-order.hs
@@ -3,8 +3,8 @@
 fun1' :: [Integer] -> Integer
 fun1' = product . map (subtract 2) . filter even
 
--- fun2' :: Integer -> Integer
--- fun2' = 
+fun2' :: Integer -> Integer
+fun2' = sum . filter even . takeWhile (/= 1) . iterate (\x -> if even x then x `div` 2 else 3 * x + 1)
 
 -- Supplied functions
 
