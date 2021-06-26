ed5b6248e4b62d3ec661490afb54f7bfe297ee76
Adam Morris <aimorris@aimorris.com>
2021-06-27 08:12:32 +1000

    Reformat

--- 04-higher-order/04-higher-order.hs
+++ 04-higher-order/04-higher-order.hs
@@ -1,12 +1,26 @@
--- Main functions
+-- Main
 
 fun1' :: [Integer] -> Integer
-fun1' = product . map (subtract 2) . filter even
+fun1' =
+  product .
+  map (subtract 2) .
+  filter even
 
 fun2' :: Integer -> Integer
-fun2' = sum . filter even . takeWhile (/= 1) . iterate (\x -> if even x then x `div` 2 else 3 * x + 1)
+fun2' =
+  sum .
+  filter even .
+  takeWhile (/= 1) .
+  iterate (\x -> if even x then x `div` 2 else 3 * x + 1)
 
--- Supplied functions
+foldTree :: [a] -> Tree a
+foldTree = error "null"
+
+-- Supplied
+
+data Tree a = Leaf
+              | Node Integer (Tree a) a (Tree a)
+  deriving (Show, Eq)
 
 fun1 :: [Integer] -> Integer
 fun1 [] = 1
