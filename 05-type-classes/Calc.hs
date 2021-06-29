712b7c9b76f0d392a2ed9b236219d957535557fa
Adam Morris <aimorris@aimorris.com>
2021-06-29 16:06:06 +1000

    05-type-classes

---
+++ 05-type-classes/Calc.hs
@@ -0,0 +1,10 @@
+import ExprT
+import Parser
+
+eval :: ExprT -> Integer
+eval (Lit x) = x
+eval (Add x y) = eval x + eval y
+eval (Mul x y) = eval x * eval y
+
+evalStr :: String -> Maybe Integer
+evalStr = fmap eval . parseExp Lit Add Mul
