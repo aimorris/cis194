a28fc18d0f96a5e5aaa95c9cb73f97a78d183cde
Adam Morris <aimorris@aimorris.com>
2021-07-13 09:15:23 +1000

    ex 1

---
+++ 08-io/Party.hs
@@ -0,0 +1,16 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Party where
+
+import Employee ( GuestList(..), Employee(empFun) )
+
+-- Exercise 1
+
+glCons :: Employee -> GuestList -> GuestList
+glCons emp (GL l f) = GL (emp : l) (f + empFun emp)
+
+instance Monoid GuestList where
+  mempty = GL [] 0
+
+instance Semigroup GuestList where
+  (<>) (GL a b) (GL c d) = GL (a ++ c) (b + d) 
