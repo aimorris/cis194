596f2a9da7b44545a491a0bf074fce830ecd8725
Adam Morris <aimorris@aimorris.com>
2021-07-13 18:18:33 +1000

    fix treefold and complete exercise 4

--- 08-io/Party.hs
+++ 08-io/Party.hs
@@ -14,12 +14,24 @@ instance Monoid GuestList where
   mempty = GL [] 0
 
 instance Semigroup GuestList where
-  (<>) (GL a b) (GL c d) = GL (a ++ c) (b + d) 
+  (<>) (GL a b) (GL c d) = GL (a ++ c) (b + d)
 
 moreFun :: GuestList -> GuestList -> GuestList
 moreFun a b = if a > b then a else b
 
 -- Exercise 2
 
-treeFold :: (b -> Tree a -> b) -> b -> Tree a -> b
-treeFold f acc tree = foldl f (f acc tree) $ subForest tree
+treeFold :: (a -> [b] -> b) -> b -> Tree a -> b
+treeFold f acc tree = f (rootLabel tree) $ map (treeFold f acc) (subForest tree)
+
+-- Exercise 3
+
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel boss gls = (withBoss, withoutBoss)
+  where withoutBoss = maximum $ map fst gls
+        withBoss = maximum $ map (glCons boss . snd) gls
+
+-- Exercise 4
+
+maxFun :: Tree Employee -> GuestList
+maxFun = uncurry moreFun . treeFold nextLevel (mempty, mempty)
